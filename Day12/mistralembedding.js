b04378f3e625d5cf7bbc293e94c75d752c2fdcd0
imrajat-sharma <rajat.s082005@gmail.com>
2026-08-30 11:41:14 +0530

    added comments

--- Day12/mistralembedding.js
+++ Day12/mistralembedding.js
@@ -1,12 +1,13 @@
 import {MistralAIEmbeddings} from '@langchain/mistralai';
 import Config from '../config.js'
 
+//API key check
 const apiKey = Config.apiKey
 if (!apiKey) {
   throw new Error("Mistral API key is not found!");
 }
 
-
+//Embedding
 const embedding = new MistralAIEmbeddings({
   model:'mistral-embed',
   apiKey:apiKey
